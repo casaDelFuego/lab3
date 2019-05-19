8f65b0ada0e6076d66f6966e827fd219f8e4aa1b
Olga <tselyuk.olga@gmail.com>
2019-05-19 22:41:50 +0200

    first commit, database has been created

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC33-tVoG0SIn3-5jSQGn2fD-8c7Pfz_Ro",
+  authDomain: "lab3-47dd6.firebaseapp.com",
+  databaseURL: "https://lab3-47dd6.firebaseio.com",
+  projectId: "lab3-47dd6",
+  storageBucket: "lab3-47dd6.appspot.com",
+  messagingSenderId: "742555563206",
+  appId: "1:742555563206:web:a57c171468c00a77"
+};
+
+export default firebaseConfig;
